194634a22f7ced8fbc5b8f744be4c50e3078351b
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-02-01 22:46:16 +0530

    updated user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -19,6 +19,8 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+//this is a pre-save hook!! This will be executed before the saving a document to the MongoDB database.
+
 userSchema.pre("save", async function(next){
     try {
         if(this.isModified('password')){
@@ -34,7 +36,10 @@ userSchema.pre("save", async function(next){
     }
 })
 
+
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcryptjs.compare(password, this.password);
 }
+
+
 export const User = mongoose.model('User',userSchema)
