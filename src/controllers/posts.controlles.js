36a236e032e7b681db78f9459ed5aa3127c26cd2
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-03-08 19:44:09 +0530

    post

--- src/controllers/posts.controlles.js
+++ src/controllers/posts.controlles.js
@@ -1,5 +1,12 @@
 import { uploadOnClodinary } from "../utils/cloudinary";
 import Post from "../models/post.models"
 import ApiResponse from "../utils/ApiResponse";
+import asyncHandler from "../utils/asyncHandler";
+import ApiError from "../utils/ApiErrors.js";
 
+const createPosts = asyncHandler(async (req,res,next)=>{
 
+if(!req.user.isAdmin){
+    throw new ApiError("You are not alloed to create a new post")
+}
+})
