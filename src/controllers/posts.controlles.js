e8249ef37e33f46f59c7896a92ec6ea7e7555c2c
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-03-10 11:20:58 +0530

    create and delete functionality added

--- src/controllers/posts.controlles.js
+++ src/controllers/posts.controlles.js
@@ -9,4 +9,71 @@ const createPosts = asyncHandler(async (req,res,next)=>{
 if(!req.user.isAdmin){
     throw new ApiError("You are not alloed to create a new post")
 }
+if(!req.body.title|| !req.body.content){
+    throw new ApiError(400,"Please provide all required fields")
+}
+
+const slug = req.body.title
+    .split(' ')
+    .join('-')
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, '');
+
+const newPost = new Post({
+    ...req.body,
+    slug,
+    Author:req.user.id,
+});
+const savedPost = await newPost.save()
+res.status(201).json(savedPost)
 })
+
+
+const getPosts = asyncHandler(async (req, res, next) => {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortingDirection = req.query.order === "asc" ? 1 : -1;
+  
+    const posts = await Post.find({
+      ...(req.query.Author && { Author: req.query.Author }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortingDirection })
+      .skip(startIndex)
+      .limit(limit);
+  
+    const totalPosts = await Post.countDocuments();
+  
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+  
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+  
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  });
+  
+
+
+export {
+
+    createPosts,
+    getPosts
+
+}
