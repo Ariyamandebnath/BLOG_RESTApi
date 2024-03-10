d2018b22ad135b463a5f0a804065b54359f9aabf
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-03-10 14:00:12 +0530

    updated new elements  in comments schema

--- src/models/comments.models.js
+++ src/models/comments.models.js
@@ -1,16 +1,32 @@
 import mongoose from 'mongoose';
 
-const commentSchema= new mongoose.Schema({
-    content:{
-        type: String,
-        required: true,
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
     },
-    author:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+    postId: {
+      type: String,
+      required: true,
     },
+    userId: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    numberOfLikes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps: true});
+
+
 
 export const Comment = mongoose.model('Comment',commentSchema)
